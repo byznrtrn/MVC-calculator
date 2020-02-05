7981d19fcdb85007eb51bd3411e8fd317cde4def
Beyza <32273312+byznrtrn@users.noreply.github.com>
2020-02-05 19:52:48 +0300

    Add files via upload

---
+++ MVCHW1/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCHW1.Models;
+
+namespace MVCHW1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Process(Calculator calculator)
+        {
+
+            if (calculator.Concat != null)
+            {
+                calculator.Field += calculator.Concat;
+            }
+            else if (calculator.ClearAll != null)
+            {
+                calculator.Field = "";
+            }
+            else if (calculator.Clear != null)
+            {
+                calculator.Field = calculator.Field.Substring(0, calculator.Field.Length - 1);
+            }
+            else if (calculator.Calculate != null)
+            {
+                calculator.Field = new DataTable().Compute(calculator.Field, null).ToString();
+            }
+            else if (calculator.Sign != null)
+            {
+                calculator.Field = "-" + calculator.Field;
+            }
+
+            return View("Index", calculator);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCHW1/Models/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCHW1.Models
+{
+    public class Calculator
+    {
+        public string Field { get; set; }
+        public string Concat { get; set; }
+        public string ClearAll { get; set; }
+        public string Clear { get; set; }
+        public string Sign { get; set; }
+        public string Calculate { get; set; }
+    }
+}
